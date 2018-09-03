7d5125d66f530ae80da1702fe9db8ee47a2c9ed6
dydhyh <dydhyh@163.com>
2018-09-03 22:31:12 +0800

    重构MessageManage， 将类中Switch语句重构。 Command负责处理接收/发送包，对于包的具体处理放到具体的Command中。

---
+++ QQ.FrameworkTest/Domains/Commands/ReceiveCommands/Login/LoginPingPacketCommandSpecs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Machine.Specifications;
+using QQ.Framework;
+using QQ.Framework.Domains;
+using QQ.Framework.Domains.Commands.ReceiveCommands.Login;
+using QQ.Framework.Utils;
+
+namespace QQ.FrameworkTest.Domains.Commands.ReceiveCommands.Login
+{
+    public class LoginPingPacketCommandSpecs
+    {
+        private Establish that =
+            () =>
+            {
+                var user = new QQUser(Int32.MaxValue, "");
+                subject = DispatchPacketToCommand.of(new byte[] { }, new QQClient() { QQUser = user });
+                command = QQCommand.Login0x0825;
+            };
+
+        private Because of =
+            () =>
+            {
+                receive_command = subject.dispatch_receive_packet(command);
+            };
+
+        private It receive_command_should_a_LoginPingCommand =
+            () =>
+            {
+                (receive_command is LoginPingCommand).ShouldBeTrue();
+            };
+
+        private static DispatchPacketToCommand subject;
+        private static QQCommand command;
+        private static ReceiveCommand receive_command;
+    }
+}
