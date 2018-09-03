882d045b8d2d9e579a2cfb4a905acd3882402c34
luojinfang <417085811@qq.com>
2018-09-04 00:02:02 +0800

    Create MyUser.cs

---
+++ QQLoginTest/MyUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QQLoginTest
+{
+    public partial class Program
+    {
+        public static long MyQQ { get; set; }
+        public static string MyPassWord { get; set; }
+    }
+}
