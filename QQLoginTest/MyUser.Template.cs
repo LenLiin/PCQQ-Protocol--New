46c249e9d8f59780d900062bd98e61c924112154
罗金方 <417085811@qq.com>
2018-09-03 23:46:37 +0800

    同步代码

--- QQLoginTest/MyUser.Template.cs
+++
@@ -1,8 +0,0 @@
-namespace QQLoginTest
-{
-    public partial class Program
-    {
-        private static readonly string MyPassWord = ""; // 密码
-        private static readonly long MyQQ = 0; // QQ号
-    }
-}
