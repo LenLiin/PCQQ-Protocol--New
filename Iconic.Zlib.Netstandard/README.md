739c0a9435ed2c58c9e39d689a7fd901532762df
罗金方 <417085811@qq.com>
2018-09-04 15:52:31 +0800

    引入https://github.com/HelloKitty/Iconic.Zlib.Netstandard

---
+++ Iconic.Zlib.Netstandard/README.md
@@ -0,0 +1,21 @@
+# Iconic.Zlib.Netstandard
+
+Iconic.Zlib.Netstandard is an open-source .NETSTANDARD, .NETCORE, and .NETFRAMEWORK compatible port of the Iconic.Zlib library.
+
+It does not contain a multithreaded decompression stream due to netstandard1.3 not having the Threadpool.
+
+## Builds
+
+Available on a Nuget: https://www.nuget.org/packages/Iconic.Zlib.Netstandard
+
+## Tests
+
+TODO Setup Mono tests
+
+#### Windows - Unit Tests
+
+(Done locally)
+
+## Licensing
+
+My changes/diff are licensed under MIT. The original Dotnetzip source's [license can be found here](http://dotnetzip.codeplex.com/license) and I'm also obligated to license this new version under the original license.
---
+++ Iconic.Zlib.Netstandard/tests/Iconic.Zlib.Netstandard.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Iconic.Zlib.Netstandard.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Iconic.Zlib.Netstandard.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2326229-f498-4bb2-8768-6559b3bf119a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
