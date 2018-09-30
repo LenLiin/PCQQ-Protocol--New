529e41a4abb74df641c0df5a8aef59c8047506d1
SGKoishi <rshyys@hotmail.com>
2018-09-29 21:27:12 -0700

    给示例机器人添加简单的反应用于测试。

--- QQLoginTest/Robots/TestRoBot.cs
+++ QQLoginTest/Robots/TestRoBot.cs
@@ -14,10 +14,19 @@ namespace QQLoginTest.Robots
         public override void ReceiveFriendMessage(long friendNumber, Richtext content)
         {
             Console.WriteLine($"机器人收到来自{friendNumber}的消息{content}");
+            if (content.ToString().StartsWith("~"))
+            {
+                _service.SendToFriend(friendNumber, content);
+            }
         }
 
         public override void ReceiveGroupMessage(long groupNumber, long fromNumber, Richtext content)
         {
+            Console.WriteLine($"机器人收到来自{groupNumber}的{fromNumber}的消息{content}");
+            if (content.ToString().StartsWith("~"))
+            {
+                _service.SendToGroup(groupNumber, content);
+            }
         }
     }
 }
